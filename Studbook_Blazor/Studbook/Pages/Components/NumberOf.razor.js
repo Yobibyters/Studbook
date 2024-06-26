625f802054cd46c23077617d29064afbc1bcdb47
Oyne <andreoyy.litvinovv@gmail.com>
2024-06-26 18:56:27 +0300

    NumberOf updown logic and style added

---
+++ Studbook_Blazor/Studbook/Pages/Components/NumberOf.razor.js
@@ -0,0 +1,11 @@
+﻿export class NumberOf {
+    static Init(input, up, down) {
+        up.addEventListener('click', e => {
+            input.stepUp()
+        });
+
+        down.addEventListener('click', e => {
+            input.stepDown()
+        });
+    }
+}
