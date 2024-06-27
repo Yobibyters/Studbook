8dc2c14395ce11203dff2831bc8a5e1a952b85d3
Anton <anton260903@gmail.com>
2024-06-27 20:58:00 +0300

    Subject model added

---
+++ Studbook_Blazor/Studbook/Models/Subject.cs
@@ -0,0 +1,9 @@
+﻿namespace Studbook.Models
+{
+    public class Subject
+    {
+        public string? Name { get; set; }
+        public double? Credit { get; set; }
+        public double? Mark { get; set; }
+    }
+}
