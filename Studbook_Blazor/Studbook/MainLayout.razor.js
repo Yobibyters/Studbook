52371eda1056eeb10fa814f36b07092671ae64d5
Anton <anton260903@gmail.com>
2024-06-30 00:54:48 +0300

    menu updated in MainLayout

--- Studbook_Blazor/Studbook/MainLayout.razor.js
+++ Studbook_Blazor/Studbook/MainLayout.razor.js
@@ -1,65 +1,24 @@
-﻿const dropdownBtn = document.querySelectorAll(".dropdown-btn");
-const dropdown = document.querySelectorAll(".dropdown");
-const hamburgerBtn = document.getElementById("hamburger");
-const navMenu = document.querySelector(".menu");
-const links = document.querySelectorAll(".dropdown a");
+﻿export class MainLayout {
+    static Init(hamburger, menu) {
+        let dropdownBtns = menu.querySelectorAll('.dropdown-btn');
+        dropdownBtns.forEach((item) => {
+            let dropdownElems = menu.querySelectorAll('.dropdown');
+            dropdownElems.forEach((elem) => {
+                elem.addEventListener("click", (e) => {
+                    e.stopPropagation();
+                });
+            });
 
-function setAriaExpandedFalse() {
-    dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
-}
-
-function closeDropdownMenu() {
-    dropdown.forEach((drop) => {
-        drop.classList.remove("active");
-        drop.addEventListener("click", (e) => e.stopPropagation());
-    });
-}
-
-function toggleHamburger() {
-    navMenu.classList.toggle("show");
-}
-
-dropdownBtn.forEach((btn) => {
-    btn.addEventListener("click", function (e) {
-        const dropdownIndex = e.currentTarget.dataset.dropdown;
-        const dropdownElement = document.getElementById(dropdownIndex);
-
-        dropdownElement.classList.toggle("active");
-        dropdown.forEach((drop) => {
-            if (drop.id !== btn.dataset["dropdown"]) {
-                drop.classList.remove("active");
-            }
+            item.addEventListener("click", () => {
+                item.classList.toggle("show");
+            });
         });
-        e.stopPropagation();
-        btn.setAttribute(
-            "aria-expanded",
-            btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
-        );
-    });
-});
 
-// close dropdown menu when the dropdown links are clicked
-links.forEach((link) =>
-    link.addEventListener("click", () => {
-        closeDropdownMenu();
-        setAriaExpandedFalse();
-        toggleHamburger();
-    })
-);
-
-// close dropdown menu when you click on the document body
-document.documentElement.addEventListener("click", () => {
-    closeDropdownMenu();
-    setAriaExpandedFalse();
-});
-
-// close dropdown when the escape key is pressed
-document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-        closeDropdownMenu();
-        setAriaExpandedFalse();
+        hamburger.addEventListener('click', () => {
+            menu.classList.toggle("show");
+            dropdownBtns.forEach((item) => {
+                item.classList.remove("show");
+            });
+        });
     }
-});
-
-// toggle hamburger menu
-hamburgerBtn.addEventListener("click", toggleHamburger);
+}
