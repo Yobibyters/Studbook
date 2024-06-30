408d04e9a052343699f9092392d633e064008c21
Anton <anton260903@gmail.com>
2024-06-30 23:55:56 +0300

    CalculationService added

---
+++ Studbook_Blazor/Studbook/Interfaces/ICalculationService.cs
@@ -0,0 +1,10 @@
+﻿using Studbook.Models;
+
+namespace Studbook.Interfaces
+{
+    public interface ICalculationService
+    {
+        public event EventHandler<double> RatingChanged;
+        public double CalculateRating(IEnumerable<Subject> subjects);
+    }
+}
