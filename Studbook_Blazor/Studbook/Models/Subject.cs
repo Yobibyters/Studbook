0633edf72f04df5bd1ace92748ee7de718c270da
Anton <anton260903@gmail.com>
2024-06-30 23:56:38 +0300

    static fields added to Subject model

--- Studbook_Blazor/Studbook/Models/Subject.cs
+++ Studbook_Blazor/Studbook/Models/Subject.cs
@@ -2,6 +2,11 @@
 {
     public class Subject
     {
+        public static readonly double CreditMax = 10.0;
+        public static readonly double CreditMin = 0.0;
+        public static readonly double MarkMax = 100.0;
+        public static readonly double MarkMin = 0.0;
+
         public string? Name { get; set; }
         public double? Credit { get; set; }
         public double? Mark { get; set; }
