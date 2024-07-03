63d2d7aba101216cb839da849805f4a724517863
Anton <anton260903@gmail.com>
2024-07-03 21:14:40 +0300

    ExportService added

--- Studbook_Blazor/Studbook/Program.cs
+++ Studbook_Blazor/Studbook/Program.cs
@@ -11,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ICalculationService, CalculationService>();
+builder.Services.AddScoped<IExportService, ExportService>();
 
 await builder.Build().RunAsync();
