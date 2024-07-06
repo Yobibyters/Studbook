bd0e55100c5bfd4ba53a507d39e91758e5b49b22
mythter <60883514+mythter@users.noreply.github.com>
2024-07-06 14:53:02 +0000

    Deploying to gh-pages from @ Yobibyters/Studbook@99847801fd0124e23fc429700bf8e83cef5fdbc6 🚀

---
+++ Pages/Components/NumericUpDown.razor.js
@@ -0,0 +1,14 @@
+﻿export class NumericUpDown {
+    static Init(input, up, down) {
+        up.addEventListener('click', e => {
+            input.stepUp();
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+
+        });
+
+        down.addEventListener('click', e => {
+            input.stepDown();
+            input.dispatchEvent(new Event('input', { bubbles: true }));
+        });
+    }
+}
---
+++ js/savers.js
@@ -0,0 +1,32 @@
+﻿export function SaveToClipboard(element) {
+    domtoimage.toBlob(element)
+        .then(function (blob) {
+            navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })])
+        });
+}
+
+export function SaveToPng(element) {
+    domtoimage.toPng(element)
+        .then(function (dataUrl) {
+            let link = document.createElement('a');
+            link.download = 'rating.png';
+            link.href = dataUrl;
+            link.click();
+        })
+        .catch(function (error) {
+            console.error('Something went wrong!', error);
+        });
+}
+
+export function SaveToJpeg(element) {
+    domtoimage.toJpeg(element, { quality: 0.95 })
+        .then(function (dataUrl) {
+            let link = document.createElement('a');
+            link.download = 'rating.jpeg';
+            link.href = dataUrl;
+            link.click();
+        })
+        .catch(function (error) {
+            console.error('Something went wrong!', error);
+        });
+}
