25d054689c76f45a36930e824f0743dd89796666
Anton <anton260903@gmail.com>
2024-07-30 23:51:56 +0300

    NumericUpDown scroll added

--- Studbook_Blazor/Studbook/Pages/Components/NumericUpDown.razor.js
+++ Studbook_Blazor/Studbook/Pages/Components/NumericUpDown.razor.js
@@ -10,5 +10,17 @@
             input.stepDown();
             input.dispatchEvent(new Event('input', { bubbles: true }));
         });
+
+        input.addEventListener('wheel', function (event) {
+            event.preventDefault();
+            if (event.deltaY < 0) {
+                input.stepUp();
+                input.dispatchEvent(new Event('input', { bubbles: true }));
+            }
+            else if (event.deltaY > 0) {
+                input.stepDown();
+                input.dispatchEvent(new Event('input', { bubbles: true }));
+            }
+        });
     }
 }
